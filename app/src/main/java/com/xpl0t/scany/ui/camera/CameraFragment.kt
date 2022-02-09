5324b3dc16531fae2b71db29c9716c68c7b10cce
xpl0t <sheepr4ider@gmail.com>
2022-02-09 07:08:41 +0100

    Added camera fragment with preview

---
+++ app/src/main/java/com/xpl0t/scany/ui/camera/CameraFragment.kt
@@ -0,0 +1,95 @@
+package com.xpl0t.scany.ui.camera
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.xpl0t.scany.R
+import com.xpl0t.scany.extensions.finish
+import com.xpl0t.scany.ui.common.BaseFragment
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class CameraFragment : BaseFragment(R.layout.camera_fragment) {
+
+    private val cameraPreview by lazy { requireView().findViewById<PreviewView>(R.id.camPreview) }
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), REQUIRED_PERMISSIONS.toTypedArray(), REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != REQUEST_CODE_PERMISSIONS) return
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            Snackbar.make(requireView(), R.string.missing_permission_err, Snackbar.LENGTH_SHORT)
+                .show()
+            finish()
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(cameraPreview.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        const val TAG = "CameraFragment"
+        private val REQUIRED_PERMISSIONS = listOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+}
