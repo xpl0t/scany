2fe6b37ebd9845b4bc84b2714fe587fe7e81594c
xpl0t <sheepr4ider@gmail.com>
2022-02-10 18:26:40 +0100

    Added improve fragment base

--- app/src/main/java/com/xpl0t/scany/ui/improve/ImproveFragment.kt
+++ app/src/main/java/com/xpl0t/scany/ui/improve/ImproveFragment.kt
@@ -1,4 +1,4 @@
-package com.xpl0t.scany.ui.crop
+package com.xpl0t.scany.ui.improve
 
 import android.graphics.Bitmap
 import android.os.Bundle
@@ -9,59 +9,50 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.xpl0t.scany.R
 import com.xpl0t.scany.extensions.finish
-import com.xpl0t.scany.extensions.showFragment
 import com.xpl0t.scany.ui.camera.CameraFragment
 import com.xpl0t.scany.ui.common.BaseFragment
-import com.xpl0t.scany.ui.improve.ImproveFragment
+import com.xpl0t.scany.ui.crop.CropFragment
 
-class CropFragment : BaseFragment(R.layout.crop_fragment) {
+class ImproveFragment : BaseFragment(R.layout.improve_fragment) {
 
     private lateinit var bitmapPreview: ImageView
     private lateinit var applyCropBtn: FloatingActionButton
-
-    private lateinit var sourceBitmap: Bitmap
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
 
-        val bitmap = arguments?.getParcelable<Bitmap>(CameraFragment.SOURCE_BITMAP)
-        if (bitmap == null) {
-            Log.e(TAG, "No source bitmap supplied")
+        val sourceBitmap = arguments?.getParcelable<Bitmap>(CameraFragment.SOURCE_BITMAP)
+        val cropBitmap = arguments?.getParcelable<Bitmap>(CropFragment.CROP_BITMAP)
+        if (sourceBitmap == null || cropBitmap == null) {
+            Log.e(TAG, "No source/crop bitmap supplied")
             Snackbar.make(requireView(), R.string.error_msg, Snackbar.LENGTH_SHORT).show()
             finish()
             return
         }
 
-        setSourceBitmap(bitmap)
+        setCropBitmap(cropBitmap)
     }
 
     private fun initViews() {
         bitmapPreview = requireView().findViewById(R.id.bitmapPreview)
-        applyCropBtn = requireView().findViewById(R.id.applyCrop)
+        applyCropBtn = requireView().findViewById(R.id.applyImprove)
 
         applyCropBtn.setOnClickListener {
-            Log.d(TAG, "Apply crop btn clicked")
-            showImproveFragment(sourceBitmap)
+            Log.d(TAG, "Apply improve btn clicked")
         }
     }
 
-    private fun setSourceBitmap(bitmap: Bitmap) {
-        sourceBitmap = bitmap
+    private fun setCropBitmap(bitmap: Bitmap) {
         bitmapPreview.setImageBitmap(bitmap)
     }
 
-    private fun showImproveFragment(bitmap: Bitmap) {
-        val bundle = Bundle().apply {
-            putParcelable(CameraFragment.SOURCE_BITMAP, sourceBitmap)
-            putParcelable(CROP_BITMAP, bitmap)
-        }
-
-        parentFragmentManager.showFragment(ImproveFragment(), true, bundle)
+    private fun finishWithImprovedImage(improved: Bitmap) {
+        finish()
     }
 
     companion object {
-        const val TAG = "CropFragment"
-        const val CROP_BITMAP = "CROP_BITMAP"
+        const val TAG = "ImproveFragment"
+        const val IMPROVED_BITMAP = "IMPROVED_BITMAP"
     }
 }
