57cf73ff7c963719d32aef72c6eb9482c10a8632
Johannes Weih <sheepr4ider@gmail.com>
2022-04-10 15:55:25 +0200

    feat: added stopwatch for profiling

---
+++ app/src/main/java/com/xpl0t/scany/util/Stopwatch.kt
@@ -0,0 +1,36 @@
+package com.xpl0t.scany.util
+
+import java.lang.System.currentTimeMillis
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.ExperimentalTime
+
+/**
+ * Measure time.
+ */
+class Stopwatch {
+
+    private var startTime: Long? = null
+
+    /**
+     * Start the stopwatch.
+     */
+    fun start() {
+        startTime = currentTimeMillis()
+    }
+
+    /**
+     * Stop the stopwatch & return the elapsed time in milliseconds.
+     * If it has never been started, returns 0.
+     *
+     * @return Elapsed time in milliseconds, or 0 if it was never started.
+     */
+    fun stop(): Long {
+        if (startTime == null) {
+            return 0
+        }
+
+        return currentTimeMillis() - startTime!!
+    }
+
+}
