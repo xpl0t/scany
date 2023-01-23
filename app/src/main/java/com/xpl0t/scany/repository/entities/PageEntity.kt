49ab1f9504833745ba89ce3f4539d585362d27ff
Johannes Weih <sheepr4ider@gmail.com>
2023-01-23 19:48:49 +0100

    build(gradle): upgrade

--- app/src/main/java/com/xpl0t/scany/repository/entities/PageEntity.kt
+++ app/src/main/java/com/xpl0t/scany/repository/entities/PageEntity.kt
@@ -3,7 +3,7 @@ package com.xpl0t.scany.repository.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
-import androidx.room.ForeignKey.CASCADE
+import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.PrimaryKey
 
 @Entity(
